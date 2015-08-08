aad8f3de39db9b7ccc5c1cbfffb45d4bd9b414f9
Chris Lee <c_leeonidas41@yahoo.com>
2015-08-07 21:29:18 -0400

    user controller added

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+  
+  def index
+    @users = User.all 
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def update
+    if current_user.update_attributes(user_params)
+      flash[:notice] = "User information updated"
+      redirect_to user_path
+    else
+      flash[:error] = "Invalid User Information"
+      redirect_to edit_user_registration_path
+    end
+  end
+
+  private 
+
+  def user_params
+    params.require(:user).permit(:name)
+  end
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   
   devise_for :users
+  resources :users, only: [:index,:show,:update]
   
   get 'welcome/index'
 
